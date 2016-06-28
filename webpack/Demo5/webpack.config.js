484b842b76a65d8360511875a9e4cf7beea9e0cd
huliyou <liyoupie@gmail.com>
2016-06-28 17:15:28 +0800

    webpack add Demo5

---
+++ webpack/Demo5/webpack.config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by leiyouwho on 6/28/16.
+ */
+
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'}
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('This is webpack demo!')
+  ]
+}
